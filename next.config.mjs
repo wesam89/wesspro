55a7cff674f40694fa3eb088acf3d1255dc38847
wessam alhawamda <wesohawa5@gmail.com>
2025-06-15 14:50:36 -0500

    Configured redirects to www.wesspro.ca

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,28 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+/**
+* @type {import('next').NextConfig}
+*/
+const nextConfig = {
+async redirects() {
+return [
+{
+source: '/:path*',
+has: [{ type: 'host', value: 'wesspro.vercel.app' }],
+destination: 'https://www.wesspro.ca/:path*',
+permanent: true,
+},
+{
+source: '/:path*',
+has: [{ type: 'host', value: 'wesspro.ca' }],
+destination: 'https://www.wesspro.ca/:path*',
+permanent: true,
+},
+{
+source: '/index.php',
+destination: '/',
+permanent: true,
+},
+];
+},
+};
 
 export default nextConfig;
